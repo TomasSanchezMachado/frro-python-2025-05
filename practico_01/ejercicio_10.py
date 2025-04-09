11f5c150cbec71c1b38aefa2bec6da0ea70b7e6e
Nicolás Escobar <nicoescobar666@gmail.com>
2025-04-09 15:03:51 -0300

    Resolución ejercicio 10
    Co-Authored-By: Vittorio Maragliano <136398543+maraglianovittorio@users.noreply.github.com>
    Co-Authored-By: Tomas Sanchez Machado <108631674+TomasSanchezMachado@users.noreply.github.com>

--- practico_01/ejercicio_10.py
+++ practico_01/ejercicio_10.py
@@ -6,7 +6,13 @@ from typing import Iterable
 def tiene_pares_basico(numeros: Iterable[int]) -> bool:
     """Toma una lista y devuelve un booleano en función si tiene al menos un
     número par."""
-    pass # Completar
+    for i in numeros:
+        if (i % 2 == 0):
+            return True
+    return False
+    
+    
+    
 
 
 # NO MODIFICAR - INICIO
@@ -23,7 +29,13 @@ def tiene_pares_for_else(numeros: Iterable[int]) -> bool:
     """Re-Escribir utilizando for-else con dos return y un break.
     Referencia: https://docs.python.org/3/tutorial/controlflow.html#break-and-continue-statements-and-else-clauses-on-loops
     """
-    pass # Completar
+    for i in numeros:
+        if (i % 2 == 0):
+            break
+       
+    else:
+        return False
+    return True
 
 
 # NO MODIFICAR - INICIO
@@ -40,8 +52,12 @@ def tiene_pares_any(numeros: Iterable[int]) -> bool:
     """Re-Escribir utilizando la función any, sin utilizar bucles.
     Referencia: https://docs.python.org/3/library/functions.html#any
     """
-    pass # Completar
-
+    def any():
+        for i in numeros:
+            if (i % 2 == 0):
+                return True
+        return False
+    return any()
 
 # NO MODIFICAR - INICIO
 assert tiene_pares_any([1, 3, 5]) is False
